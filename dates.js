e30d5e69f540f462d620c7bb9c8010d611c65989
Harshvardhan Raj <harsh18092004vardhan@gmail.com>
2026-08-23 01:51:52 +0530

    Update practices

--- dates.js
+++ dates.js
@@ -1,3 +1,12 @@
 let myDate = new Date();
-console.log(myDate);
-console.log(myDate.toString());
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let myDate1 = new Date(2024, 5, 20, 10 ,30, 0);
+// console.log(myDate1.toString());
+// console.log(myDate1.toLocaleString());
+// console.log(myDate1.toDateString());
+// console.log(myDate1.toTimeString());
