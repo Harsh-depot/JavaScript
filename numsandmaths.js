67c7668d1f109b2e66f9a3504c10659abfff174f
Harshvardhan Raj <harsh18092004vardhan@gmail.com>
2026-08-23 01:33:26 +0530

    Update practices

---
+++ numsandmaths.js
@@ -0,0 +1,34 @@
+// const score = 400
+
+// const balance = new Number (1000)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const othernumber = 23.8999
+
+// console.log(othernumber.toPrecision(2));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// ++++++++++++++++++++++ Maths ++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4, 2, 8, 1));
+// console.log(Math.max(4, 2, 8, 1));
+
+// console.log(Math.random());
+
+
+
+// console.log(Math.floor(Math.random()*10 +1));
+
+const  min = 1;
+const max = 6;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // 1 to 6 as in a dice.
